d7760498ba03291dbc8e81ef0f22f84cde95e86f
Hariswaran Sitaraman <hsitaram@el3.ib0.cm.hpc.nrel.gov>
2022-08-05 12:07:57 -0600

    adding regression testing scripts

---
+++ tests/script_reg_testing.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+TOPDIR=${PWD}
+
+#number of processors to use
+NPROCS=${1:-16}
+
+#need mpirun command (on eagle it is srun and 
+#peregrine it is mpirun)
+MPI_RUN_COMMAND=${2:-srun}
+
+#script that is used to set environment variables for
+#post processing or doing conda related commands
+SCRIPTFILE=${3:-~/ytenv.sh}
+
+RESULTS_DIR=${4:-~/mflo_results}
+
+MAKEFILE_OPTIONS=${5:-}
+rm -rf ${RESULTS_DIR}
+
+declare -a allcases=('Channel' 'ShockTube_N2' 'ShockTube_N2He' 'SpeciesEqTests')
+
+#clean directories
+for case in "${allcases[@]}";
+do
+	cd ${case}
+        make realclean
+        rm -rf finalpl* *.png
+        . ../clean.sh
+        cd ${TOPDIR}
+done
+
+#run cases
+for case in "${allcases[@]}";
+do
+        echo ${case}
+	cd ${case}
+        make -j ${MAKEFILE_OPTIONS} 
+        . ./runcase.sh "${MPI_RUN_COMMAND}" ${NPROCS}
+        cd ${TOPDIR}
+done
+
+source ${SCRIPTFILE}
+mkdir ${RESULTS_DIR}
+
+#post process
+for case in "${allcases[@]}";
+do
+	cd ${case}
+        . ./verifycase.sh
+        mv *.png ${RESULTS_DIR}
+        cd ${TOPDIR}
+done
+
+cd ${RESULTS_DIR}
+convert *.png regtest_results.pdf
