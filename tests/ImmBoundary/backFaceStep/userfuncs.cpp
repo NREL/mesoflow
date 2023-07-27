cb6608c71580bcd843317100ccf46d5a0164e658
Hariswaran Sitaraman <hsitaram@el1.ib0.cm.hpc.nrel.gov>
2023-07-27 14:41:09 -0600

    1) adding bface step case files 2) adding descriptive abort when dsdt is nan

---
+++ tests/ImmBoundary/backFaceStep/userfuncs.cpp
@@ -0,0 +1,23 @@
+#include<userfuncs.H>
+#include <AMReX_ParmParse.H>
+
+namespace mflo_user_funcs
+{
+    AMREX_GPU_DEVICE_MANAGED Real lscale=1.0;
+    AMREX_GPU_DEVICE_MANAGED Real fs_vel=0.2366;
+    AMREX_GPU_DEVICE_MANAGED Real Re=40.0;
+    AMREX_GPU_DEVICE_MANAGED Real rho0=1.0;
+    AMREX_GPU_DEVICE_MANAGED Real p0=1.0;
+
+    void initialize_problem()
+    {
+        Print()<<"Initializing problem\n";
+    
+        ParmParse pp("user");
+        pp.query("fs_vel",fs_vel);
+        pp.query("lscale",lscale);
+        pp.query("Re",Re);
+        pp.query("rho0",rho0);
+        pp.query("p0",p0);
+    }
+}
